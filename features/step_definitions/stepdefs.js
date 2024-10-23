be826f19062d37184b0fe461221a1f00bcc7ce8c
felipe valeriano <felipevalerianodosreis@gmail.com>
2024-10-23 18:02:55 -0300

    fix: stepdefs

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,2 +1,22 @@
 const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('que sou estudante', function () {
+    // Write code here that turns the phrase above into concrete actions       
+    return 'pending';
+  });
+
+  When('deseja cadastrar no app memorinha', function () {
+    // Write code here that turns the phrase above into concrete actions       
+    return 'pending';
+  });
+
+  When('digito um email errado', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('o app envia a mensagem {string}', function (string) {  
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
